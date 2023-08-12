7ff6083c0ffc94177892ee485362f08b521045ac
DenserMeerkat <95911940+DenserMeerkat@users.noreply.github.com>
2023-08-12 23:36:30 +0530

    Update Fields

--- components/Fields.tsx
+++ components/Fields.tsx
@@ -19,31 +19,20 @@ const Test = () => {
   } = state;
   return (
     <div className="max-w-7xl mx-auto pt-10 py-6 px-2 sm:px4 md:px-6">
-      <div className="mx-auto max-w-[12rem] h-10 border border-zinc-300 dark:border-zinc-800 rounded-md flex">
-        <div className="relative">
-          <CustomInput defaultVal={days} />
-          <p className="absolute top-3 right-4 text-xs text-zinc-500 ">
-            Day(s)
-          </p>
-        </div>
-        <Separator orientation="vertical" />
-        <div className="relative">
-          <CustomInput defaultVal={hours} />
-          <p className="absolute top-3 right-4 text-xs text-zinc-500 ">
-            Hour(s)
-          </p>
-        </div>
+      <div className="mx-auto max-w-[12rem] h-10 flex">
+        <CustomInput
+          defaultVal={days}
+          direction={"l"}
+          suffix={"Day(s)"}
+          updateContext={updateDays}
+        />
+        <CustomInput
+          defaultVal={hours}
+          direction={"r"}
+          suffix={"Hour(s)"}
+          updateContext={updateHours}
+        />
       </div>
-      {/* <div className="flex flex-row gap-3 sm:gap-8 justify-center">
-        <div className="max-w-[9rem] md:max-w-[8rem] sm:grid grid-cols-2 flex flex-col gap-2 sm:gap-6 sm:items-center">
-          <p className="pl-1 ">Days</p>
-          <Input type="number" defaultValue={days} className="" />
-        </div>
-        <div className="max-w-[9rem] md:max-w-[8rem] sm:grid grid-cols-2 flex flex-col gap-2 sm:gap-6 sm:items-center">
-          <p className="pl-1 ">Hours</p>
-          <Input type="number" defaultValue={hours} className="" />
-        </div>
-      </div> */}
     </div>
   );
 };
--- components/common/CustomInput.tsx
+++ components/common/CustomInput.tsx
@@ -3,25 +3,37 @@ import { Input } from "@/components/ui/input";
 
 const CustomInput = (props: any) => {
   const defaultVal = props.defaultValue;
+  const direction: string = props.direction;
+  const suffix: string = props.suffix;
+  const updateContext = props.updateContext;
   const [inputValue, setInputValue] = useState(defaultVal);
-
+  const commonClasses =
+    "ml-2 m-0 pl-4 rounded-none border-0 focus-visible:ring-0 focus-visible:bg-zinc-100 focus-visible:dark:bg-zinc-900 focus-visible:border-zinc-400 focus-visible:dark:border-zinc-500 ";
+  const directionalClass = `border border-zinc-300 dark:border-zinc-800 
+  ${direction === "l" ? "rounded-l-lg w-[5.7rem]" : "rounded-r-lg w-[6.1rem]"}`;
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
 
     if (/^\d*$/.test(newValue)) {
       if (newValue.length <= 2) {
         setInputValue(newValue);
+        updateContext(newValue);
       }
     }
   };
   return (
-    <Input
-      type="number"
-      value={inputValue}
-      placeholder="0"
-      onChange={handleInputChange}
-      className="ml-2 m-0 pl-3 rounded-none border-0 focus-visible:ring-0 focus-visible:bg-zinc-100 focus-visible:dark:bg-zinc-900"
-    />
+    <div className="relative">
+      <Input
+        type="number"
+        value={inputValue}
+        placeholder="0"
+        onChange={handleInputChange}
+        className={`${commonClasses} ${directionalClass}`}
+      />
+      <p className="absolute top-3 right-4 text-xs text-zinc-500 pointer-events-none">
+        {suffix}
+      </p>
+    </div>
   );
 };
 
