d5d83bdf73095378d4d123f7387710f9e4f23492
DenserMeerkat <95911940+DenserMeerkat@users.noreply.github.com>
2023-08-13 00:09:45 +0530

    Updated Field Size

--- components/common/CustomInput.tsx
+++ components/common/CustomInput.tsx
@@ -8,9 +8,9 @@ const CustomInput = (props: any) => {
   const updateContext = props.updateContext;
   const [inputValue, setInputValue] = useState(defaultVal);
   const commonClasses =
-    "ml-2 m-0 pl-4 rounded-none border-0 focus-visible:ring-0 focus-visible:bg-zinc-100 focus-visible:dark:bg-zinc-900 focus-visible:border-zinc-400 focus-visible:dark:border-zinc-500 ";
-  const directionalClass = `border border-zinc-300 dark:border-zinc-800 
-  ${direction === "l" ? "rounded-l-lg w-[5.7rem]" : "rounded-r-lg w-[6.1rem]"}`;
+    "ml-2 m-0 pl-3 h-12 rounded-none border-0 focus-visible:ring-0 focus-visible:bg-zinc-100 focus-visible:dark:bg-zinc-900 focus-visible:border-zinc-400 focus-visible:dark:border-zinc-500 text-lg font-semibold";
+  const directionalClass = `border-2 border-zinc-200 dark:border-zinc-800 
+  ${direction === "l" ? "rounded-l-lg w-[6.8rem]" : "rounded-r-lg w-[7.2rem]"}`;
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
 
@@ -27,10 +27,11 @@ const CustomInput = (props: any) => {
         type="number"
         value={inputValue}
         placeholder="0"
+        autoFocus
         onChange={handleInputChange}
         className={`${commonClasses} ${directionalClass}`}
       />
-      <p className="absolute top-3 right-4 text-xs text-zinc-500 pointer-events-none">
+      <p className="absolute top-3 right-4 text-md text-zinc-500 pointer-events-none">
         {suffix}
       </p>
     </div>
